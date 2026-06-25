7bf74d71e0e8ef1e6152516c40cd759b2dd0107e
Udo Friman-Gayer <udo.friman-gayer@maxiv.lu.se>
2026-06-25 11:22:52 +0200

    Abstract Level class
    - Towards a more systematic definition of the hierarchy in getmax.

---
+++ mctools/common/risk/level.py
@@ -0,0 +1,47 @@
+"""Hierarchy of logical levels with associated sublevels"""
+
+from abc import abstractmethod
+
+from value import UnknownValue, Value
+
+
+class BaseLevel:
+    """Base level without sublevels"""
+
+    def __init__(self, name: str = "", title: str = ""):
+        self.name = name
+        if self.name != "" and title == "":
+            self.title = name
+        self.value: Value = UnknownValue()
+
+    def get_max_value(self) -> Value:
+        """Retrieve the maximum value. If unknown, evaluate it first."""
+        if isinstance(self.value, UnknownValue):
+            self.evaluate()
+        return self.value
+
+    @abstractmethod
+    def evaluate(self):
+        """Evaluate the maximum value"""
+
+
+class Level(BaseLevel):
+    """Upper and intermediate level with sublevels"""
+
+    def __init__(
+        self,
+        name: str = "",
+        title: str = "",
+        sub_levels: dict[str, "Level"] | None = None,
+    ):
+        super().__init__(name=name, title=title)
+        if sub_levels is None:
+            self.sub_levels: dict[str, Level] = {}
+        else:
+            self.sub_levels = sub_levels
+
+    def evaluate(self):
+        """Find the maximum value of all sublevels"""
+        self.value = max(
+            sub_level.get_max_value() for sub_level in self.sub_levels.items()
+        )
