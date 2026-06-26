16da02699aa8de995d07be3daf4fa4e5c049a2f4
Udo Friman-Gayer <udo.friman-gayer@maxiv.lu.se>
2026-06-27 00:04:02 +0200

    Fix ROOT histogram indexing in tests

--- mctools/common/risk/test/test_zone.py
+++ mctools/common/risk/test/test_zone.py
@@ -6,65 +6,65 @@ from mctools.common.risk.test.input_histogram import create_test_histogram
 
 class TestZone(unittest.TestCase):
     def test_bin_in_range(self):
-        hist = create_test_histogram()
+        hist = create_test_histogram(name="th3_0")
         axis = hist.GetXaxis()
         with self.assertWarns(UserWarning):
-            bin_in_range(n_bin=0, axis=axis, limits=Limits(lower=-0.3, upper=-0.7))
+            bin_in_range(n_bin=1, axis=axis, limits=Limits(lower=-0.3, upper=-0.7))
         # Both limits inside bin
         self.assertTrue(
-            bin_in_range(n_bin=0, axis=axis, limits=Limits(lower=-0.7, upper=-0.3))
+            bin_in_range(n_bin=1, axis=axis, limits=Limits(lower=-0.7, upper=-0.3))
         )
         # Lower limit outside bin
         self.assertTrue(
-            bin_in_range(n_bin=0, axis=axis, limits=Limits(lower=-1.3, upper=-0.3))
+            bin_in_range(n_bin=1, axis=axis, limits=Limits(lower=-1.3, upper=-0.3))
         )
         # Upper limit outside bin
         self.assertTrue(
-            bin_in_range(n_bin=0, axis=axis, limits=Limits(lower=-0.7, upper=0.3))
+            bin_in_range(n_bin=1, axis=axis, limits=Limits(lower=-0.7, upper=0.3))
         )
         # Equal limits inside bin
         self.assertTrue(
-            bin_in_range(n_bin=0, axis=axis, limits=Limits(lower=-0.1, upper=-0.1))
+            bin_in_range(n_bin=1, axis=axis, limits=Limits(lower=-0.1, upper=-0.1))
         )
         # Equal limits on edges
         self.assertTrue(
-            bin_in_range(n_bin=0, axis=axis, limits=Limits(lower=-1.0, upper=-1.0))
+            bin_in_range(n_bin=1, axis=axis, limits=Limits(lower=-1.0, upper=-1.0))
         )
         self.assertTrue(
-            bin_in_range(n_bin=0, axis=axis, limits=Limits(lower=0.0, upper=0.0))
-        )
-        # Both limits outside bin
-        self.assertTrue(
-            bin_in_range(n_bin=0, axis=axis, limits=Limits(lower=-10.0, upper=10.0))
-        )
-
-        # Both limits below bin
-        self.assertFalse(
-            bin_in_range(n_bin=1, axis=axis, limits=Limits(lower=-1.0, upper=-0.5))
-        )
-        self.assertFalse(
-            bin_in_range(n_bin=1, axis=axis, limits=Limits(lower=-0.1, upper=-0.1))
-        )
-        # Both limits above bin
-        self.assertFalse(
-            bin_in_range(n_bin=1, axis=axis, limits=Limits(lower=1.5, upper=2.5))
-        )
-        # Upper limit on edge
-        self.assertTrue(
-            bin_in_range(n_bin=1, axis=axis, limits=Limits(lower=-1.0, upper=0.0))
-        )
-        # Lower limit on edge
-        self.assertTrue(
-            bin_in_range(n_bin=1, axis=axis, limits=Limits(lower=1.0, upper=2.0))
+            bin_in_range(n_bin=1, axis=axis, limits=Limits(lower=0.0, upper=0.0))
         )
         # Both limits outside bin
         self.assertTrue(
             bin_in_range(n_bin=1, axis=axis, limits=Limits(lower=-10.0, upper=10.0))
         )
 
+        # Both limits below bin
+        self.assertFalse(
+            bin_in_range(n_bin=2, axis=axis, limits=Limits(lower=-1.0, upper=-0.5))
+        )
+        self.assertFalse(
+            bin_in_range(n_bin=2, axis=axis, limits=Limits(lower=-0.1, upper=-0.1))
+        )
+        # Both limits above bin
+        self.assertFalse(
+            bin_in_range(n_bin=2, axis=axis, limits=Limits(lower=1.5, upper=2.5))
+        )
+        # Upper limit on edge
+        self.assertTrue(
+            bin_in_range(n_bin=2, axis=axis, limits=Limits(lower=-1.0, upper=0.0))
+        )
+        # Lower limit on edge
+        self.assertTrue(
+            bin_in_range(n_bin=2, axis=axis, limits=Limits(lower=1.0, upper=2.0))
+        )
+        # Both limits outside bin
+        self.assertTrue(
+            bin_in_range(n_bin=2, axis=axis, limits=Limits(lower=-10.0, upper=10.0))
+        )
+
     def test_zone(self):
         # Create test histogram
-        hist = create_test_histogram()
+        hist = create_test_histogram(name="th3_1")
 
         # 1) No Condition
         zone = Zone(hist=hist)
